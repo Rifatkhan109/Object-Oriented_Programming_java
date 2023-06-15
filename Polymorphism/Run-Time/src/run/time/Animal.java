8458ab21c2cde1decc2772ac6ab657ea3859753d
Md. Rifat Khan <120739312+Rifatkhan109@users.noreply.github.com>
2023-06-15 22:10:05 +0600

    Add files via upload

---
+++ Polymorphism/Run-Time/src/run/time/Animal.java
@@ -0,0 +1,7 @@
+package run.time;
+
+public class Animal {
+      public void makeSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
---
+++ Polymorphism/Run-Time/src/run/time/Dog.java
@@ -0,0 +1,8 @@
+
+package run.time;
+
+public class Dog extends Animal {
+        public void makeSound() {
+        System.out.println("The dog barks");
+    }
+}
