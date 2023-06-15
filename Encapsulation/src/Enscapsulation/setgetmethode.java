6bb75c3f88e573aababa2a47be86bf10d43673e9
Md. Rifat Khan <120739312+Rifatkhan109@users.noreply.github.com>
2023-06-15 22:08:53 +0600

    Add files via upload

---
+++ Encapsulation/src/Enscapsulation/setgetmethode.java
@@ -0,0 +1,16 @@
+
+package enscapsulation;
+
+
+public class setgetmethode {
+    private String name;
+private int age;
+void setName( String name){
+    this.name=name;
+    
+}
+    String getName(){
+        return name;
+    } 
+
+}
---
+++ Encapsulation/src/Enscapsulation/test.java
@@ -0,0 +1,13 @@
+
+package enscapsulation;
+
+
+public class test {
+    public static void main(String[] args) {
+        setgetmethode r=new setgetmethode();
+        r.setName( "rifat");
+        System.out.println(r.getName());
+        
+    }
+    
+}
